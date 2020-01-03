62ec8138ce968c707022aec8dc9dac6e487b1389
Fridolin Pokorny <fridolin.pokorny@gmail.com>
2020-01-03 14:11:05 +0100

    Happy new year!

--- pytorch/matmul.py
+++ pytorch/matmul.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright(C) 2019 Francesco Murdaca
+# Copyright(C) 2019, 2020 Francesco Murdaca
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
